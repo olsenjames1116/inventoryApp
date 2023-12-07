b23eb502daad0fe5a2a2c8b8c96ed5265991adaf
James Olsen <olsenjames1116@gmail.com>
2023-12-07 18:28:45 -0500

    Add inventory route to app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const inventoryRouter = require('./routes/inventory');
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
