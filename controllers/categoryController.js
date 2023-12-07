0e9e52578d7cd002be48229367e0fad946c6d387
James Olsen <olsenjames1116@gmail.com>
2023-12-07 13:51:04 -0500

    Change _ syntax to camelCase

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,41 +2,41 @@ const Category = require('../models/category');
 const asyncHandler = require('express-async-handler');
 
 // Display a list of all Category.
-exports.category_list = asyncHandler((req, res, next) => {
+exports.categoryList = asyncHandler((req, res, next) => {
 	res.send('NOT IMPLEMENTED: Category list');
 });
 
 // Display detail page for a specific Category.
-exports.category_detail = asyncHandler((req, res, next) => {
+exports.categoryDetail = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
 });
 
 // Display Category create form on GET.
-exports.category_create_get = asyncHandler((req, res, next) => {
+exports.categoryCreateGet = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category create GET`);
 });
 
 // Display Category create form on POST.
-exports.category_create_post = asyncHandler((req, res, next) => {
+exports.categoryCreatePost = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category create POST`);
 });
 
 // Display Category delete form on GET.
-exports.category_delete_get = asyncHandler((req, res, next) => {
+exports.categoryDeleteGet = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category delete GET`);
 });
 
 // Display Category delete form on POST.
-exports.category_delete_post = asyncHandler((req, res, next) => {
+exports.categoryDeletePost = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category delete POST`);
 });
 
 // Display Category update form on GET.
-exports.category_update_get = asyncHandler((req, res, next) => {
+exports.categoryUpdateGet = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category update GET`);
 });
 
 // Display Category update form on POST.
-exports.category_update_post = asyncHandler((req, res, next) => {
+exports.categoryUpdatePost = asyncHandler((req, res, next) => {
 	res.send(`NOT IMPLEMENTED: Category update POST`);
 });
--- routes/inventory.js
+++ routes/inventory.js
@@ -2,32 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 // Require controller modules.
-const category_controller = require('../controllers/categoryController');
+const categoryController = require('../controllers/categoryController');
 
-// CATEGORY ROUTES
+// CATEGORY ROUTES //
 
 // GET request for creating a Category.
-router.get('/category/create', category_controller.category_create_get);
+router.get('/category/create', categoryController.categoryCreateGet);
 
 // POST request for creating a Category.
-router.post('/category/create', category_controller.category_create_post);
+router.post('/category/create', categoryController.categoryCreatePost);
 
 // GET request to delete Category.
-router.get('/category/:id/delete', category_controller.category_delete_get);
+router.get('/category/:id/delete', categoryController.categoryDeleteGet);
 
 // POST request to delete Category.
-router.post('/category/:id/delete', category_controller.category_delete_post);
+router.post('/category/:id/delete', categoryController.categoryDeletePost);
 
 // GET request to update Category.
-router.get('/category/:id/update', category_controller.category_update_get);
+router.get('/category/:id/update', categoryController.categoryUpdateGet);
 
 // POST request to update Category.
-router.post('/category/:id.update', category_controller.category_update_post);
+router.post('/category/:id.update', categoryController.categoryUpdatePost);
 
 // GET request for one Category.
-router.get('/category/:id', category_controller.category_detail);
+router.get('/category/:id', categoryController.categoryDetail);
 
 // GET request for list of all Category.
-router.get('/categories', category_controller.category_list);
+router.get('/categories', categoryController.categoryList);
 
 module.exports = router;
