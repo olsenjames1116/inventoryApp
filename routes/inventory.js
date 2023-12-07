bf48022d890b17a5df0c52cb811e5f88806f6dd0
James Olsen <olsenjames1116@gmail.com>
2023-12-07 18:24:24 -0500

    Fill inventory router with routes to item controller

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,8 +3,36 @@ const router = express.Router();
 
 // Require controller modules.
 const categoryController = require('../controllers/categoryController');
+const itemController = require('../controllers/itemController');
 
 // ITEM ROUTES //
+
+// GET inventory home page.
+router.get('/', itemController.index);
+
+// GET request for creating an Item.
+router.get('/item/create', itemController.itemCreateGet);
+
+// POST request for creating an Item.
+router.post('/item/create', itemController.itemCreatePost);
+
+// GET request to delete an Item.
+router.get('/item/:id/delete', itemController.itemDeleteGet);
+
+// POST request to delete an Item.
+router.post('/item/:id/delete', itemController.itemDeletePost);
+
+// GET request to update an Item.
+router.get('/item/:id/update', itemController.itemUpdateGet);
+
+// POST request to update an Item.
+router.post('/item/:id/update', itemController.itemUpdatePost);
+
+// GET request for one Item.
+router.get('/item/:id', itemController.itemDetail);
+
+// GET request for a list of all items.
+router.get('/items', itemController.itemList);
 
 // CATEGORY ROUTES //
 
